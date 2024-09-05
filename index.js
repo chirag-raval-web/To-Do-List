8ef6ce0d37f2d546de8aa4847b6eb22494f6b0c8
CHIRAG RAVAL <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-05 20:28:42 +0530

    adding bootstrap for design

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ addbtn.addEventListener("click", () => {
     let div = document.createElement("div");
     div.classList.add("items");
     div.innerHTML =
-        `<input type="text" name="text" id="item1">
-                    <i class="fa-solid fa-pen-to-square updatebtn"
-                        id="update1"></i><i
-                        class="fa-solid fa-trash removebtn"
-                        id="remove1"></i>`;
+        `                <div class="form-group col-11 ms-3 d-flex gap-2">
+                    <input type="text" class="form-control  p-2 shadow  "
+                        placeholder="Create more ! we have no limits">
+                    
+                    <i
+                        class="icons p-3 ps-4 pe-4 shadow border-dark  rounded text-light fa-solid fa-floppy-disk ms-3 w-2 d-flex align-items-center btn-success"></i>
+                    <i
+                        class="icons p-3  ps-4 pe-4 shadow border-dark rounded fa-solid fa-pen-to-square updatebtn ms-3 w-2 d-flex align-items-center btn-primary"></i>
+                    <i
+                        class="icons p-3  ps-4 pe-4 shadow border-dark rounded fa-solid fa-trash removebtn ms-3 w-2 d-flex align-items-center btn-danger letter-spaccing"></i>
+                </div>`;
     list.appendChild(div);
     console.log(div);
 });
