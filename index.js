ff28f0956ed81e9dd6160e985fa58c8580ec0608
CHIRAG RAVAL <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-05 21:17:21 +0530

    added js logic proper append item with id

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 
 let addbtn = document.getElementById("addbtn");
+
 let list = document.getElementById("list-here");
 console.log(list);
-
+let conter = 2;
 addbtn.addEventListener("click", () => {
     let div = document.createElement("div");
     div.classList.add("items");
+    
     div.innerHTML =
-        `                <div class="form-group col-11 ms-3 d-flex gap-2">
-                    <input type="text" class="form-control  p-2 shadow  "
-                        placeholder="Create more ! we have no limits">
-                    
-                    <i
+        `              <div class="form-group col-11 ms-3 d-flex gap-2" id="list-group-${conter}">
+                    <input type="text" class="form-control  p-2 shadow  " id="item-${conter}"
+                        placeholder="Create your First To-Do-List..">
+
+                    <i  id="saveBtn${conter}"
                         class="icons p-3 ps-4 pe-4 shadow border-dark  rounded text-light fa-solid fa-floppy-disk ms-3 w-2 d-flex align-items-center btn-success"></i>
-                    <i
-                        class="icons p-3  ps-4 pe-4 shadow border-dark rounded fa-solid fa-pen-to-square updatebtn ms-3 w-2 d-flex align-items-center btn-primary"></i>
-                    <i
-                        class="icons p-3  ps-4 pe-4 shadow border-dark rounded fa-solid fa-trash removebtn ms-3 w-2 d-flex align-items-center btn-danger letter-spaccing"></i>
-                </div>`;
+                    <i id="updateBtn${conter}"
+                        class="icons p-3  ps-4 pe-4 shadow border-dark rounded fa-solid fa-pen-to-square  ms-3 w-2 d-flex align-items-center btn-primary"></i>
+                    <i id="deleteBtn${conter}"
+                        class="icons p-3  ps-4 pe-4 shadow border-dark rounded fa-solid fa-trash  ms-3 w-2 d-flex align-items-center btn-danger letter-spaccing"></i>
+                </div> `;
     list.appendChild(div);
-    console.log(div);
+    conter++;
 });
+
