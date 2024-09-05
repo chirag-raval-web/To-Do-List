82665507afb7c9109c2dcf7821f608d835c414e1
CHIRAG RAVAL <131503978+chirag-raval-web@users.noreply.github.com>
2024-09-05 08:43:48 +0530

    created design for To-Do-List

---
+++ index.js
@@ -0,0 +1,23 @@
+// {/* <input type="text" name="text" id="item1">
+// <i class="fa-solid fa-pen-to-square update"
+//     id="update1"></i><i
+//     class="fa-solid fa-trash remove"
+//     id="remove1"></i>
+// </div> */}
+
+let addbtn = document.getElementById("addbtn");
+let list = document.getElementById("list-here");
+console.log(list);
+
+addbtn.addEventListener("click", () => {
+    let div = document.createElement("div");
+    div.classList.add("items");
+    div.innerHTML =
+        `<input type="text" name="text" id="item1">
+                    <i class="fa-solid fa-pen-to-square updatebtn"
+                        id="update1"></i><i
+                        class="fa-solid fa-trash removebtn"
+                        id="remove1"></i>`;
+    list.appendChild(div);
+    console.log(div);
+});
